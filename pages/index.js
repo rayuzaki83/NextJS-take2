5013c48831108973ece03ebacb169c97408dd80b
rayusaki83 <rayyap247@gmail.com>
2022-03-27 20:16:51 +0100

    various files amended

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <h1 className="text-3xl font-bold underline text-sky-500 ">
-      My NextJS App!
+      My New Shopify Store is open!
     </h1>
   );
 }
